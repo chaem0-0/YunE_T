32cc3ab820df021f5f9ff289b77d0486f8f32d66
HYUN <8135zxcvbn@gmail.com>
2022-12-19 15:20:35 +0900

    index(완성)

--- Assets/Script/Back_ground.cs
+++ Assets/Script/Back_ground.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Back_ground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
+
 }
